fa65fc8631dc1dcc59e66bf62e14c8b62ec5292c
moiify <neoxtl@outlook.com>
2020-01-11 14:10:19 +0800

    A commit a day keeps the girlfriend away.

--- Auto.bat
+++ Auto.bat
@@ -1,8 +1,7 @@
 @echo off
 @title Auto Green Point
 
-echo %date:~0,10% %time%>>CommitTime.txt
-echo.>CommitTime.txt
+echo %date:~0,10% %time%^>>CommitTime.txt
 git add .
 git commit -m "A commit a day keeps the girlfriend away."
 git push -u origin master
