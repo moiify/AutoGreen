1d5824bc1f062bad2be493ae5d01ddd6da55e31d
moiify <neoxtl@outlook.com>
2020-03-29 23:49:14 +0800

    A commit a day keeps the girlfriend away.

--- auto.sh
+++ auto.sh
@@ -1,6 +1,6 @@
 #！/bin/bash
 #This is a simple script.
-echo %date:~0,10% %time%^>CommitTime.txt
+echo $(date)>>CommitTime.txt
 git add .
 git commit -m "A commit a day keeps the girlfriend away."
 git push -u origin master
