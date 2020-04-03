45934bd91131640ab97ff9029b84e1586f47c0b4
moiify <neoxtl@outlook.com>
2020-04-04 00:06:01 +0800

    A commit a day keeps the girlfriend away.

--- linux-auto.sh
+++ linux-auto.sh
@@ -2,7 +2,7 @@
 cd /home/AutoGreen/
 echo $(date)>>CommitTime.txt
 git add .
-git commit -m "A commit a dat keeps the girlfriend away"
+git commit -m "A commit a day keeps the girlfriend away."
 git push -u origin master
 echo I'M Stronger..
 
