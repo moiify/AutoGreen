52c07534e9450830d9c583cc94f6207ceeb78585
neoxin <neoxtl@outlook.com>
2023-09-19 13:16:51 +0000

    A commit a day keeps the girlfriend away.

--- linux-auto.sh
+++ linux-auto.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
-cd /home/AutoGreen/
-echo $(date)>>CommitTime.txt
+echo $(date)>>./CommitTime.txt
 git add .
 git commit -m "A commit a day keeps the girlfriend away."
 git push -u origin master
