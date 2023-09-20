4cc71cb7990e819f4364612eed302a213fa6f615
neoxin <neoxtl@outlook.com>
2023-09-20 03:39:45 +0000

    A commit a day keeps the girlfriend away.

--- linux-auto.sh
+++ linux-auto.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-SCRIPT_DIR = "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$SCRIPT_DIR"
 echo $(date)>>./CommitTime.txt
 git add .
